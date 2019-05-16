d1528f7a2a5c1009613c165184013fca746631a7
JD <jdhouse27@gmail.com>
2019-05-16 18:08:47 -0500

    got user input to push to an object

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,23 +5,14 @@ module.exports = function(app){
     return res.json(friends);
         });
   
-  
-  
 //   All possible Friends JSON input
   app.post("/api/friends", function(req, res) {
       // req.body hosts is equal to the JSON post sent from the user
       // This works because of our body parsing middleware
-    //   let newReservation = req.body;
-    // 
-    //   console.log(newReservation);
-//   
-        //   if (reservations.length < 5){
-            //   reservations.push(newReservation);
-        //   } else {           
-            //   waitList.push(newReservation); 
-        //   }      
-        //   res.json(newReservation);
-    //   
+      let userData = req.body;
+      console.log(userData);
+   
+        res.json(userData);  
       });
       
 }
