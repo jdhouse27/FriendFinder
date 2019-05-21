5c5b1373193c161a0a4097d943749ed6be83cb2f
JD <jdhouse27@gmail.com>
2019-05-20 21:47:48 -0500

    final mods --got app to working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,8 +9,36 @@ module.exports = function(app){
   app.post("/api/friends", function(req, res) {
       // req.body hosts is equal to the JSON post sent from the user
       // This works because of our body parsing middleware
+      let match = {
+        name: "",
+        photo: "",
+        friendDifference: 100
+      };
+      
+      
       let userData = req.body;
-      console.log(userData);
+      let userScores = userData.scores;
+      let totalDifference;
+  
+      for (var i = 0; i < friends.length; i++) {
+        let currentFriend = friends[i];
+        totalDifference = 0;
+  
+       for (var j = 0; j < currentFriend.scores.length; j++) {
+          let currentFriendScore = currentFriend.scores[j];
+          let currentUserScore = userScores[j];
+  
+          totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+        }
+  
+        if (totalDifference <= bestMatch.friendDifference) {
+          bestMatch.name = currentFriend.name;
+          bestMatch.photo = currentFriend.photo;
+          bestMatch.friendDifference = totalDifference;
+        }
+      }
+  
+        friends.push(userData);
    
         res.json(userData);  
       });
